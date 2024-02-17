47322052b691f74eb97a7a943d085b9f6b36a7c2
Joe Zakielarz <jjzfive@gmail.com>
2024-02-17 15:10:25 -0800

    Add tailwindcss

--- apps/extension/popup.tsx
+++ apps/extension/popup.tsx
@@ -1,19 +1,18 @@
 import { useState } from "react";
 
+import "style.css";
+
 function IndexPopup() {
 	const [data, setData] = useState("");
 
 	return (
-		<div
-			style={{
-				padding: 16,
-			}}>
-			<h2>
+		<div className="bg-blue-500 h-80 px-80">
+			<h2 className="bg-blue-500">
 				Welcome to your{" "}
 				<a href="https://www.plasmo.com" target="_blank">
 					Plasmo
 				</a>{" "}
-				Extension!
+				Extension!sss
 			</h2>
 			<input onChange={(e) => setData(e.target.value)} value={data} />
 			<a href="https://docs.plasmo.com" target="_blank">
