ee0733f0143561cc8a855a0b4bb997da2789cf2e
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-02-17 13:06:07 -0800

    formatting

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,9 +1,10 @@
 import { Elysia } from "elysia";
-import { swagger } from '@elysiajs/swagger'
+import { swagger } from "@elysiajs/swagger";
 
-const app = new Elysia().use(swagger())
-  .post("/articles", () => "Insert article")
-  .post("/specific_points", () => "Insert specific_point")
-  .put("/specific_points", () => "Update relations superset_point_id")
-  .post("/superset_points", () => "Insert superset_point")
-  .listen(3000);
+const app = new Elysia()
+	.use(swagger())
+	.post("/articles", () => "Insert article")
+	.post("/specific_points", () => "Insert specific_point")
+	.put("/specific_points", () => "Update relations superset_point_id")
+	.post("/superset_points", () => "Insert superset_point")
+	.listen(3000);
