694525c97e1b93f760b15d74efb39d18091f3c89
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-02-17 13:01:32 -0800

    feat: scaffold routes

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,7 +1,8 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
-
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+const app = new Elysia()
+  .post("/articles", () => "Insert article")
+  .post("/specific_points", () => "Insert specific_point")
+  .put("/specific_points", () => "Update relations superset_point_id")
+  .post("/superset_points", () => "Insert superset_point")
+  .listen(3000);
