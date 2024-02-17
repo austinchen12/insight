9f4eeb62460a8d0f28972cd2ea0cb38aaeef5150
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-02-17 13:05:55 -0800

    feat: scaffold db connection

---
+++ apps/backend/src/db/index.ts
@@ -0,0 +1,5 @@
+import { drizzle } from "drizzle-orm/libsql";
+import { createClient } from "@libsql/client";
+
+const client = createClient({ url: "http://127.0.0.1:8080" });
+export const db = drizzle(client);
