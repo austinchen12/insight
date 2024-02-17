419ebd17760b9b24b107c57a158723de13af2c08
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-02-17 14:43:06 -0800

    feat: actual insert and update

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,10 +1,22 @@
 import { Elysia } from "elysia";
 import { swagger } from "@elysiajs/swagger";
+import { db } from "./db";
+import { articles, specificPoints, supersetPoints } from "./db/schema";
 
 const app = new Elysia()
 	.use(swagger())
-	.post("/articles", () => "Insert article")
-	.post("/specific_points", () => "Insert specific_point")
-	.put("/specific_points", () => "Update relations superset_point_id")
-	.post("/superset_points", () => "Insert superset_point")
+	.post("/articles", async ({ body }) => {
+		await db.insert(articles).values(body);
+	})
+	.post("/specific_points", async ({ body }) => {
+		await db.insert(specificPoints).values(body);
+	})
+	.put("/specific_points", async ({ body }) => {
+		await db.update(specificPoints).set({
+			supersetPointId: body.supersetPointId,
+		});
+	})
+	.post("/superset_points", async ({ body }) => {
+		await db.insert(supersetPoints).values(body);
+	})
 	.listen(3000);
