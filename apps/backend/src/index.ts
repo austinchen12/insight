cd983def99dc1e494d17aa7750276cbdb079b9b1
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-02-18 14:12:08 -0800

    feat: tRPC router restored

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,11 +1,15 @@
 import { cors } from "@elysiajs/cors";
+import { initTRPC } from "@trpc/server";
 import { swagger } from "@elysiajs/swagger";
 import { TypeCompiler } from "@sinclair/typebox/compiler";
-import { Elysia, Static, TSchema, t } from "elysia";
+import { Elysia, Static, TSchema, t as Type } from "elysia";
+import { compile as c, trpc } from "@elysiajs/trpc";
+
 import {
 	SelectArticle,
 	SelectSpecificPoint,
 	SelectSupersetPoint,
+	articles,
 	insertArticles,
 	insertSpecificPoints,
 	insertSupersetPointsSchema,
@@ -13,7 +17,10 @@ import {
 	selectArticlesSchema,
 	selectSpecificPointsSchema,
 	selectSupersetPointsSchema,
+	specificPoints,
+	supersetPoints,
 } from "./db/schema";
+import { db } from "./db";
 
 export type SelectArticleJoinedSpecificPoints = SelectArticle & {
 	specificPoints: SelectSpecificPoint[];
@@ -81,46 +88,36 @@ async function execute<T extends TSchema>({
 	return parse(schema, result);
 }
 
-// const t = initTRPC.create();
-//
-// const router = t.router({
-// 	findSimilarArticles: t.procedure
-// 		.input(z.object({ url: z.string() }))
-// 		.query(({ input: { url } }) => {
-// 			// ...
-// 		}),
-// 	insertArticles: t.procedure.input(insertArticles).mutation(({ input }) => {
-// 		return execute({
-// 			sql: "INSERT INTO articles (title, url, bias, sentiment, embedding) VALUES (:title, :url, :bias, :sentiment, :embedding)",
-// 			params: input,
-// 		});
-// 	}),
-// 	insertSpecificPoints: t.procedure
-// 		.input(insertSpecificPoints)
-// 		.mutation(async ({ input }) => {
-// 			return execute({
-// 				sql: "INSERT INTO specific_points (article_id, original_excerpt, embedding, bias, sentiment, superset_point_id) VALUES (:article_id, :original_excerpt, :embedding, :bias, :sentiment, :superset_point_id)",
-// 				params: input,
-// 			});
-			// await db.insert(specificPoints).values(input);
-// 		}),
-// 	updateSpecificPoints: t.procedure
-// 		.input(insertSpecificPoints)
-// 		.mutation(async ({ input }) => {
-// 			// await db.update(specificPoints).set({
-// 			// 	superset_point_id: input.superset_point_id,
-// 			// });
-// 		}),
-// 	insertSupersetPoints: t.procedure
-// 		.input(insertSupersetPoints)
-// 		.mutation(async ({ input }) => {
-// 			return execute({
-// 				sql: "INSERT INTO superset_points (title_generated, embedding) VALUES (:title_generated, :embedding)",
-// 				params: input,
-// 			});
-// 			// await db.insert(supersetPoints).values(input);
-// 		}),
-// });
+const t = initTRPC.create();
+const p = t.procedure;
+
+const router = t.router({
+	findSimilarArticles: p
+		.input(c(Type.Object({ url: Type.String() })))
+		.query(({ input: { url } }) => {
+			// ...
+		}),
+	insertArticles: p.input(c(insertArticles)).mutation(({ input }) => {
+		return db.insert(articles).values(input);
+	}),
+	insertSpecificPoints: p
+		.input(c(insertSpecificPoints))
+		.mutation(async ({ input }) => {
+			return db.insert(specificPoints).values(input);
+		}),
+	updateSpecificPoints: t.procedure
+		.input(c(insertSpecificPoints))
+		.mutation(async ({ input }) => {
+			db.update(specificPoints).set({
+				superset_point_id: input.superset_point_id,
+			});
+		}),
+	insertSupersetPoints: t.procedure
+		.input(c(insertSupersetPointsSchema))
+		.mutation(async ({ input }) => {
+			return db.insert(supersetPoints).values(input);
+		}),
+});
 
 // export type Router = typeof router;
 
