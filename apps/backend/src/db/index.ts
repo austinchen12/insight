8f96154ff3b5edbedb0b0d3fa9788243dd5f8314
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-02-18 14:35:17 -0800

    feat: pass schema and logger true

--- apps/backend/src/db/index.ts
+++ apps/backend/src/db/index.ts
@@ -1,5 +1,9 @@
 import { drizzle } from "drizzle-orm/libsql";
 import { createClient } from "@libsql/client";
+import * as schema from "./schema";
 
 const client = createClient({ url: "http://127.0.0.1:8080" });
-export const db = drizzle(client);
+export const db = drizzle(client, {
+	schema,
+	logger: true,
+});
