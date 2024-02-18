0f2c468e85617d16ef607812e3f814daa4350743
Joe Zakielarz <jjzfive@gmail.com>
2024-02-17 16:48:18 -0800

    Scanned article

--- apps/extension/components/Summary.tsx
+++ apps/extension/components/Summary.tsx
@@ -46,8 +46,74 @@ function Summary() {
 		},
 	];
 
+	const sampleArticles = [
+		{
+			title:
+				"Billions of dollars are flowing into grants. Do we think that they can solve our problems?",
+			site: "CNN",
+			bias: "64",
+			positive: "53",
+		},
+		{
+			title:
+				"Billions of dollars are flowing into grants. Do we think that they can solve our problems?",
+			site: "CNN",
+			bias: "64",
+			positive: "53",
+		},
+		{
+			title:
+				"Billions of dollars are flowing into grants. Do we think that they can solve our problems?",
+			site: "CNN",
+			bias: "64",
+			positive: "53",
+		},
+		{
+			title:
+				"Billions of dollars are flowing into grants. Do we think that they can solve our problems?",
+			site: "CNN",
+			bias: "64",
+			positive: "53",
+		},
+		{
+			title:
+				"Billions of dollars are flowing into grants. Do we think that they can solve our problems?",
+			site: "CNN",
+			bias: "64",
+			positive: "53",
+		},
+		{
+			title:
+				"Billions of dollars are flowing into grants. Do we think that they can solve our problems?",
+			site: "CNN",
+			bias: "64",
+			positive: "53",
+		},
+		{
+			title:
+				"Billions of dollars are flowing into grants. Do we think that they can solve our problems?",
+			site: "CNN",
+			bias: "64",
+			positive: "53",
+		},
+		{
+			title:
+				"Billions of dollars are flowing into grants. Do we think that they can solve our problems?",
+			site: "CNN",
+			bias: "64",
+			positive: "53",
+		},
+		{
+			title:
+				"Billions of dollars are flowing into grants. Do we think that they can solve our problems?",
+			site: "CNN",
+			bias: "64",
+			positive: "53",
+		},
+	];
+
 	return (
-		<div className="px-4 py-2">
+		<div className="px-3 py-2">
 			<div className="flex justify-between items-center">
 				<h2 className="text-lg text-dark" style={{ fontWeight: 400 }}>
 					Summary
@@ -60,7 +126,7 @@ function Summary() {
 
 			<div className="flex justify-between items-center mt-2">
 				{/* Bias */}
-				<div className="bg-content-box h-36 w-36 rounded-lg border border-dark-green flex flex-col items-center">
+				<div className="bg-content-box h-36 w-40 rounded-lg border border-dark-green flex flex-col items-center">
 					<div className="flex items-center justify-center -mt-2">
 						<RadialBarChart
 							width={100}
@@ -88,7 +154,7 @@ function Summary() {
 				</div>
 
 				{/* Sentiment */}
-				<div className="bg-content-box h-36 w-36 rounded-lg border border-dark-green flex flex-col items-center">
+				<div className="bg-content-box h-36 w-40 rounded-lg border border-dark-green flex flex-col items-center">
 					<div className="flex items-center justify-center -mt-2">
 						<RadialBarChart
 							width={100}
@@ -113,6 +179,14 @@ function Summary() {
 				</div>
 			</div>
 
+			<h2 className="text-lg text-dark mt-2" style={{ fontWeight: 400 }}>
+				Scanned Articles
+			</h2>
+
+			{sampleArticles.map((article, idx) => (
+				<ScannedArticle article={article} key={idx} />
+			))}
+
 			{/* <Accordion type="single" collapsible className="w-full">
 				<AccordionItem value="item-1">
 					<AccordionTrigger>Is it accessible?</AccordionTrigger>
@@ -135,6 +209,21 @@ function Summary() {
 					</AccordionContent>
 				</AccordionItem>
 			</Accordion> */}
+		</div>
+	);
+}
+
+// TODO: format sentiment
+function ScannedArticle({ article }) {
+	return (
+		<div className="bg-content-box w-full hover:cursor-pointer px-2 py-1 rounded-lg border border-[#A0CA21] hover:opacity-80 mb-1">
+			<h3 className="text-[#606060] text-sm">{article.title}</h3>
+			<div className="flex gap-3 text-[#8DB613] text-xs">
+				<p>
+					{article.site} &#183; {article.bias}% bias &#183; {article.positive}%
+					positive
+				</p>
+			</div>
 		</div>
 	);
 }
